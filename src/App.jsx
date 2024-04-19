e18d3e04457c34dd0f174edc8dfbc126a2c98def
pritam-code-n-7 <pritamimca1003@gmail.com>
2024-04-19 17:21:54 +0530

    add theme provider using shadcn/ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,38 @@
 /* eslint-disable no-unused-vars */
-import React from "react"
+import React from "react";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
 import UserContextProvider from "./context/CreateContextProvider";
+import { ThemeProvider } from "./components/theme/ThemeProvider";
+import { ModeToggle } from "./components/theme/ModeToggle";
 
 function App() {
   return (
-    <UserContextProvider>
-      <div className="min-h-screen flex flex-col justify-center items-center px-4">
-        <h1 className="text-3xl font-bold underline mb-8">Hello world!</h1>
-        <div className="flex flex-col md:flex-row md:gap-8 w-full">
-          <div className="w-full">
-            <Login />
-          </div>
-          <div className="md:w-1/2">
-            <Profile />
-          </div>
+    <>
+      <ThemeProvider attribute="class" defaultTheme="system">
+        <div>
+          <UserContextProvider>
+            <div className="min-h-screen flex flex-col justify-center items-center px-4">
+              <div className="flex gap-4">
+                <h1 className="text-3xl font-bold underline mb-8">
+                  Hello world!
+                </h1>
+                <ModeToggle />
+              </div>
+
+              <div className="flex flex-col md:flex-row md:gap-8 w-full">
+                <div className="w-full">
+                  <Login />
+                </div>
+                <div className="md:w-1/2">
+                  <Profile />
+                </div>
+              </div>
+            </div>
+          </UserContextProvider>
         </div>
-      </div>
-    </UserContextProvider>
+      </ThemeProvider>
+    </>
   );
 }
 
---
+++ src/components/theme/ThemeProvider.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+
+"use client";
+
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+export function ThemeProvider({ children, ...props }) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
